06025006d90ce17400c21c2cc2a23998ac7f430d
Ignacio Furió José <bichoifj@gmail.com>
2026-05-01 01:57:47 +0200

    chore: add merchant API types

---
+++ src/types/merchant.types.ts
@@ -0,0 +1,51 @@
+export interface ShopType {
+  id: number
+  name: string
+}
+
+export interface MerchantQuality {
+  id: number
+  name: string
+}
+
+export interface Item {
+  id: number
+  name: string
+  price: string
+  quantityFormula: string
+  source: string
+  notes: string
+  shopTypeId: number
+  merchantQualityId: number
+}
+
+export interface InventoryItem {
+  itemId: number
+  quantity: number
+  finalPrice: string
+  status: string
+  notes: string
+  item: Item
+}
+
+export interface MerchantPreview {
+  name: string
+  species: string
+  region: string
+  attitude: string
+  personalityTrait: string
+  ideal: string
+  bond: string
+  flaw: string
+  gimmick: string
+  notes: string
+  shopTypeId: number
+  merchantQualityId: number
+  shopType?: ShopType
+  merchantQuality?: MerchantQuality
+}
+
+export interface Merchant extends MerchantPreview {
+  id: number
+  inventory?: InventoryItem[]
+}
